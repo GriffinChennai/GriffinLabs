717643359121c79d73cb42ea53d11e7ddc29609f
Mubarak <ibtmubarak@gmail.com>
2011-04-01 12:32:26 +0530

    Modified SilentChat.java

--- SilentChat/src/com/android/SilentChat/SilentChat.java
+++ SilentChat/src/com/android/SilentChat/SilentChat.java
@@ -8,6 +8,6 @@ public class SilentChat extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main); 
     }
 }
