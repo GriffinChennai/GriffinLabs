a8c9671392dff7c79e9e1321b01156c930b2e68e
Mubarak <ibtmubarak@gmail.com>
2011-04-07 16:14:45 +0530

    Modified SilentChat.java file

--- SilentChat/src/com/android/SilentChat/SilentChat.java
+++ SilentChat/src/com/android/SilentChat/SilentChat.java
@@ -2,10 +2,12 @@ package com.android.SilentChat;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class SilentChat extends Activity {
+	private static final int REQUEST_ENABLE_BT = 0;
 	/** Called when the activity is first created. */
 	// Local Bluetooth adapter
 	private BluetoothAdapter mBluetoothAdapter = null;
@@ -21,8 +23,20 @@ public class SilentChat extends Activity {
 		if (mBluetoothAdapter == null) {
 			Toast.makeText(this, "Bluetooth is not available",
 					Toast.LENGTH_LONG).show();
-			finish();
+			//finish();
 			return;
 		}
+		// If BT is not on, request that it be enabled.
+        // setupChat() will then be called during onActivityResult
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+            Toast.makeText(this, "Bluetooth is not Enable",
+					Toast.LENGTH_LONG).show();
+        // Otherwise, setup the chat session
+        } else {
+        	Toast.makeText(this, "Bluetooth is Enable",
+					Toast.LENGTH_LONG).show();
+        }
 	}
 }
