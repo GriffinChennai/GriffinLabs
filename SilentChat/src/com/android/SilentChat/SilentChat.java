33640f87f805483b819e066a6b1fbcf5ec31e054
Mubarak <ibtmubarak@gmail.com>
2011-04-07 13:37:56 +0530

    Added 'Bluetooth adapter' settings in silentchat.java

--- SilentChat/src/com/android/SilentChat/SilentChat.java
+++ SilentChat/src/com/android/SilentChat/SilentChat.java
@@ -1,13 +1,28 @@
 package com.android.SilentChat;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SilentChat extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main); 
-    }
+	/** Called when the activity is first created. */
+	// Local Bluetooth adapter
+	private BluetoothAdapter mBluetoothAdapter = null;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		// Get local Bluetooth adapter
+		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+		// If the adapter is null, then Bluetooth is not supported
+		if (mBluetoothAdapter == null) {
+			Toast.makeText(this, "Bluetooth is not available",
+					Toast.LENGTH_LONG).show();
+			finish();
+			return;
+		}
+	}
 }
